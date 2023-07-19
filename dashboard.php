e99c3647884381b242343b733819daa025641344
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-07-19 14:02:28 -0300

    design

---
+++ dashboard.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,300,1,0" />
+    <link rel="icon" href="assets/devchallenges.png">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <title>Dashboard | Authentication App | By David Carreño</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div class="header-image">
+                <img src="assets/devchallenges.svg" alt="devchangelles-logo">
+            </div>
+
+            <nav class="header-menu">
+
+                <img src="assets/user-image.jpg" alt="user-image" width="32px" height="32px" style="border-radius:8px ;">
+            </nav>
+        </header>
+
+        <div class="personal-info">
+            <h2 class="info-title">Personal info</h2>
+            <p class="info-content">Basic info, like your name and photo</p>
+        </div>
+
+        <div class="container2">
+            <div class="profile-and-list-container">
+                <div class="profile-container">
+                    <div class="profile-text">
+                        <h2 class="profile-title">Profile</h2>
+                        <p class="profile-content">Some info may be visible to other people</p>
+                    </div>
+
+                    <div class="button-container">
+                        <button class="profile-button">Edit</button>
+                    </div>
+                </div>
+
+                <section class="list-information-section">
+                    <div class="list-item list-photo">
+                        <span class="item-title">PHOTO</span>
+                        <img src="assets/user-image.jpg" alt="user-image" width="72px" height="72px" style="border-radius: 8px;">
+                    </div>
+
+
+
+                    <div class="list-item list-name">
+                        <span class="item-title">NAME</span>
+                        <span class="item-text">Xanthe Neal</span>
+                    </div>
+
+                    <div class="list-item list-bio">
+                        <span class="item-title">BIO</span>
+                        <span class="item-text">I am a software developer...</span>
+                    </div>
+
+                    <div class="list-item list-email">
+                        <span class="item-title">EMAIL</span>
+                        <span class="item-text">xanthe.neal@gmail.com</span>
+                    </div>
+
+                    <div class="list-item list-password">
+                        <span class="item-title">PASSWORD</span>
+                        <span class="item-text">*********</span>
+                    </div>
+                </section>
+
+            </div>
+
+            <footer>
+                <p>created by <span class="name">David Carreño</span></p>
+                <p>devchallenges.io</p>
+            </footer>
+
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,300,1,0" />
+    <link rel="icon" href="assets/devchallenges.png">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Authentication App | By David Carreño</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="app-container">
+            <div class="logo-container">
+                <img src="assets/devchallenges.svg" alt="devchangenges-logo" class="dev-logo">
+
+
+            </div>
+            <div class="title">
+
+                <h2>Join thousands of learners from around the world</h2>
+            </div>
+            <div class="content">
+                <p>Master web development by making real-life projects. There are multiple paths for you to choose</p>
+            </div>
+            <form class="index-form">
+                <input type="email" class="email" placeholder="Email"> <span class="material-symbols-outlined mail"
+                    style="color: #828282;">
+                    mail
+                </span>
+                <input type="password" class="password" placeholder="Password"><span
+                    class="material-symbols-outlined lock" style="color: #828282;">
+                    lock
+                </span>
+                <input type="submit" value="Start coding now" class="button">
+            </form>
+
+            <div class="social-media">
+                <p class="select-media">or continue with these social profile</p>
+                <div class="social-media-icons">
+                    <img src="assets/Google.svg" alt="Goolge-logo">
+                    <img src="assets/Facebook.svg" alt="Facebook-logo">
+                    <img src="assets/Twitter.svg" alt="Twitter-logo">
+                    <img src="assets/Gihub.svg" alt="Github-logo">
+                </div>
+                <p class="member">Already a member? <a href="#">Login</a></p>
+            </div>
+        </div>
+        <footer>
+            <p>created by <span class="name">David Carreño</span></p>
+            <p>devchallenges.io</p>
+        </footer>
+    </div>
+</body>
+
+</html>
